2c0535d3abe299c614593afdf0058f6c46621e72
Ajay Soni <ajay.soni@enjector.com>
2018-06-19 00:28:54 +0100

    Fix format code script

--- script/format.sh
+++ script/format.sh
@@ -1,3 +1,4 @@
-astyle -f -N -K --style=google --recursive --indent=spaces --keep-one-line-statements ../src/*.*
-astyle -f -N -K --style=google --recursive --indent=spaces --keep-one-line-statements ../include/*.*
+astyle -f -N -K --style=google --recursive --indent=spaces --keep-one-line-statements ../src/*.c
+astyle -f -N -K --style=google --recursive --indent=spaces --keep-one-line-statements ../src/*.h
+astyle -f -N -K --style=google --recursive --indent=spaces --keep-one-line-statements ../include/*.h
 find ../src -name "*.orig" -type f -delete
